15a482cb48ad20e19b445810e28fbd4362513bbb
yasisharp <77558528+yasisharp@users.noreply.github.com>
2024-09-18 21:18:59 -0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip
 
-# Upgrade setuptools and wheel to avoid build issues with numpy
-pip install --upgrade setuptools wheel
+# Upgrade setuptools to avoid issues with deprecated methods in Python 3.12
+pip install --upgrade setuptools==65.5.0  # Ensure we use a compatible version
+pip install --upgrade wheel
 
 # Upgrade pip and install virtualenv if not installed
 pip install --upgrade pip
@@ -16,7 +17,8 @@ pip install virtualenv
 virtualenv venv
 source venv/bin/activate
 
-# Install required Python packages
+# Install required Python packages, forcing the use of pre-built numpy binaries
+pip install numpy==1.24.3 --only-binary=:all:  # Force the installation from pre-built binaries
 pip install -r requirements.txt
 
 # Inform the user about activation of the virtual environment
